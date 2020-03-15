b43b6b9a0121736aee8c06b4f428ba41afa6bf52
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-15 13:59:47 +0300

    createCommit

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char symbol = 33;
+        for(int i = 33; i <= 126; i++) {
+            System.out.print(symbol +" ");
+            symbol++;
+        }
+    }
+}
---
+++ Lesson_1/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte core = 4;
+        short family = 7;
+        long model = 4820;
+        float freq = 3.7f;
+        double ram = 16.0d;
+        char m = 'i';
+        boolean isGood = true;
+        System.out.println("core "+ core +", "+ m +""+ family +"-"+ model +"K, CPU "+ freq +" GHz, RAM "+ ram +" Gb");
+        System.out.println(isGood);
+    }
+}
