dfe81b87b26d16fff4e8f73d56c805793d6cf7c9
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-15 15:07:03 +0300

    Change MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,29 +1,26 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-    boolean result = true; //index цикла
-    int question = 8; //Загаданное число
-    int answer = 15; //Первый ответ пользователя
-    
-    System.out.println("Comp: Guess my number, please");
-
-    do {
-        System.out.println("User: " + answer);
-        if (answer < question) {
-            System.out.println("Comp: No, my number is bigger. Let's again");
-            answer++;
-            result = false;
-        }
-        else if (answer > question) {
-            System.out.println("Comp: No, my number is smaller. Let's again");
-            answer--;
-            result = false;
-        }
-        else {
-            System.out.println("Comp: Yes! It's true");
-            result = true;
-        }
+        boolean result = true; //index цикла
+        int question = 8; //Загаданное число
+        int question = 8; //Загаданное число
+        int answer = 15; //Первый ответ пользователя
+        System.out.println("Comp: Guess my number, please");
+        do {
+            System.out.println("User: " + answer);
+            if (answer < question) {
+                System.out.println("Comp: No, my number is bigger. Let's again");
+                answer++;
+                result = false;
+            }
+            else if (answer > question) {
+                System.out.println("Comp: No, my number is smaller. Let's again");
+                answer--;
+                result = false;
+            }
+            else {
+                System.out.println("Comp: Yes! It's true");
+                result = true;
+            }
+        } while(result == false); //Продолжать цикл, пока результат false
     }
-    while(result == false); //Продолжать цикл, пока результат false
-
-  }
 }
