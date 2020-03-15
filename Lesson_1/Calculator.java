1e0a2fc6064d166d358f798c924589c62b0f396c
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-15 21:44:03 +0300

    Calculator

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,5 +1,42 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(" ");
+        
+        //Введенные значения пользователем:
+        int num1 = 9;  
+        int operation = '^';
+        int num2 = 3;
+
+        //Вычисление:
+        int result = 0;
+        if (operation == 43) { // Сложение
+            result = num1 + num2;
+            System.out.println(num1 +" + "+ num2 +" = "+ result);
+        }
+        else if (operation == 45) { // Вычитание
+            result = num1 - num2;
+            System.out.println(num1 +" - "+ num2 +" = "+ result);
+        }
+        else if (operation == 42) { // Умножение
+            result = num1 * num2;
+            System.out.println(num1 +" * "+ num2 +" = "+ result);
+        }
+        else if (operation == 47) { // Деление
+            result = num1 / num2;
+            System.out.println(num1 +" / "+ num2 +" = "+ result);
+        }
+        else if (operation == 94) { // Возведение в степень
+            result = num1;
+            for (int i = 1; i < num2; i++) {
+                result *= num1;
+            }
+            System.out.println(num1 +" ^ "+ num2 +" = "+ result);
+        }
+        else if (operation == 37) { // Деление по модулю
+            result = num1 % num2;
+            System.out.println(num1 +" % "+ num2 +" = "+ result);
+        }
+        else { // Неизвестная команда вычисления
+            System.out.println("Command error");
+        }
     }
 }
