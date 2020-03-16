3a3d05374ac0cd0d0376cea7cc6674612be59c8b
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-16 19:55:32 +0300

    Var correct

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -5,9 +5,10 @@ public class Variable {
         long model = 4820;
         float freq = 3.7f;
         double ram = 16.0d;
-        char m = 'i';
+        char index = 'i';
         boolean isGood = true;
-        System.out.println("core "+ core +", "+ m +""+ family +"-"+ model +"K, CPU "+ freq +" GHz, RAM "+ ram +" Gb");
+
+        System.out.println("core " + core + ", " + index + family + "-"+ model +"K, CPU "+ freq +" GHz, RAM "+ ram +" Gb");
         System.out.println(isGood);
     }
 }
