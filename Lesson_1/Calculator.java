69bb888fec6e0924551a41f2ef2607c2d72d6e4c
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-16 20:28:46 +0300

    Modif char

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -9,25 +9,25 @@ public class Calculator {
         int result = 0;
         if (operation == '+') { // Сложение
             result = num1 + num2;
-            System.out.println(num1 +" + "+ num2 +" = "+ result);
+            System.out.println(num1 + " + " + num2 + " = " + result);
         } else if (operation == '-') { // Вычитание
             result = num1 - num2;
-            System.out.println(num1 +" - "+ num2 +" = "+ result);
+            System.out.println(num1 + " - " + num2 + " = " + result);
         } else if (operation == '*') { // Умножение
             result = num1 * num2;
-            System.out.println(num1 +" * "+ num2 +" = "+ result);
+            System.out.println(num1 + " * " + num2 + " = " + result);
         } else if (operation == '/') { // Деление
             result = num1 / num2;
-            System.out.println(num1 +" / "+ num2 +" = "+ result);
+            System.out.println(num1 + " / " + num2 + " = " + result);
         } else if (operation == '^') { // Возведение в степень
             result = num1;
             for (int i = 1; i < num2; i++) {
                 result *= num1;
             }
-            System.out.println(num1 +" ^ "+ num2 +" = "+ result);
+            System.out.println(num1 + " ^ " + num2 + " = " + result);
         } else if (operation == '%') { // Деление по модулю
             result = num1 % num2;
-            System.out.println(num1 +" % "+ num2 +" = "+ result);
+            System.out.println(num1 + " % " + num2 + " = " + result);
         } else { // Неизвестная команда вычисления
             System.out.println("Command error");
         }
