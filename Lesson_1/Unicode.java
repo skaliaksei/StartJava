bd7d78fb59f4384ca0ddadfeed92f54b91a9c9ce
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-16 20:51:10 +0300

    Delete int

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,7 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
         for(char i = 33; i <= 126; i++) {
-            System.out.print(i +" ");
+            System.out.print(i + " ");
         }
     }
 }
