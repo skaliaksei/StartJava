6110751897ac3c99e3f397b36c7862407bcef781
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-16 20:45:39 +0300

    Edit variables

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,25 +1,20 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        boolean result = true; //index цикла
-        int question = 8; //Загаданное число
-        int answer = 15; //Первый ответ пользователя
+        int askNum = 8; //Загаданное число
+        int answerNum = 15; //Первый ответ пользователя
+        
         System.out.println("Comp: Guess my number, please");
         do {
-            System.out.println("User: " + answer);
-            if (answer < question) {
+            System.out.println("User: " + answerNum);
+            if (answerNum < askNum) {
                 System.out.println("Comp: No, my number is bigger. Let's again");
-                answer++;
-                result = false;
-            }
-            else if (answer > question) {
+                answerNum++;
+            } else if (answerNum > askNum) {
                 System.out.println("Comp: No, my number is smaller. Let's again");
-                answer--;
-                result = false;
+                answerNum--;
             }
-            else {
-                System.out.println("Comp: Yes! It's true");
-                result = true;
-            }
-        } while(result == false); //Продолжать цикл, пока результат false
+        } while(answerNum != askNum);
+        System.out.println("User: " + answerNum);
+        System.out.println("Comp: Yes! It's true");
     }
 }
