bcc3249e0ff5675c3639848289b7b1276fb6d4d3
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-16 20:25:49 +0300

    Modif char

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,41 +1,34 @@
 public class Calculator {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
         //Введенные значения пользователем:
         int num1 = 9;  
-        int operation = '^';
+        int operation = '*';
         int num2 = 3;
 
         //Вычисление:
         int result = 0;
-        if (operation == 43) { // Сложение
+        if (operation == '+') { // Сложение
             result = num1 + num2;
             System.out.println(num1 +" + "+ num2 +" = "+ result);
-        }
-        else if (operation == 45) { // Вычитание
+        } else if (operation == '-') { // Вычитание
             result = num1 - num2;
             System.out.println(num1 +" - "+ num2 +" = "+ result);
-        }
-        else if (operation == 42) { // Умножение
+        } else if (operation == '*') { // Умножение
             result = num1 * num2;
             System.out.println(num1 +" * "+ num2 +" = "+ result);
-        }
-        else if (operation == 47) { // Деление
+        } else if (operation == '/') { // Деление
             result = num1 / num2;
             System.out.println(num1 +" / "+ num2 +" = "+ result);
-        }
-        else if (operation == 94) { // Возведение в степень
+        } else if (operation == '^') { // Возведение в степень
             result = num1;
             for (int i = 1; i < num2; i++) {
                 result *= num1;
             }
             System.out.println(num1 +" ^ "+ num2 +" = "+ result);
-        }
-        else if (operation == 37) { // Деление по модулю
+        } else if (operation == '%') { // Деление по модулю
             result = num1 % num2;
             System.out.println(num1 +" % "+ num2 +" = "+ result);
-        }
-        else { // Неизвестная команда вычисления
+        } else { // Неизвестная команда вычисления
             System.out.println("Command error");
         }
     }
