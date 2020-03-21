5180c909aba1373bcdb41e928fa46406edf2c270
Aliaksei Skakun <skaliaksei@gmail.com>
2020-03-21 12:57:31 +0300

    PersonClass

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    int age = 20;
+    String name = "Aliaksei";
+    String sex = "man";
+    int height = 170;
+    float weight = 65.3f;
+
+    void move() {
+        System.out.println("Вы идете");
+    }
+
+    void sit() {
+        System.out.println("Вы сидите");
+    }
+
+    void run() {
+        System.out.println("Вы бежите");
+    }
+
+    boolean speak() {
+        System.out.println("Вы говорите");
+        return true;
+    }
+
+    boolean learnJava() {
+        System.out.println("Вы учите язык программирования");
+        return true;
+    }
+}
