1c927677ef640693dac6cea85921974b65ed8389
Aliaksei <skaliaksei@gmail.com>
2020-09-07 23:35:59 +0300

    New

---
+++ Lesson_2/Scanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {        
+        
+        Scanner scanNumbers = new Scanner(System.in);
+        //Scanner scanOperations = new Scanner(System.in);
+
+        int a;
+        String b;
+
+        System.out.println("Enter first number: ");
+        a = scanNumbers.nextInt();
+        // System.out.println("Enter operation: ");
+        // b = scanOperations.nextLine();
+
+        System.out.println(a);
+        //a = b;
+        // System.out.println(b);
+
+      
+
+    }
+}
